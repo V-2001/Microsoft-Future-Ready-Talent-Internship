2d4607937ddc6e935a40808e999f7f4ac96a84e9
V-2001 <78479013+V-2001@users.noreply.github.com>
2022-04-20 02:15:36 +0530

    Add files via upload

---
+++ Bot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\azurebot2906-bot-a4a2-Web-Deploy.pubxml -p:Password=bnbbg45jJWCrESXepdqQnqBnQm7dzoDGfmcfLpqic1mHNz85AKRH6Lrypwlx
+
